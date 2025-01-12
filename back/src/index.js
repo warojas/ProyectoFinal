8ab2fa55543b36144c35cdc38d35730304e90177
Raeghar12 <raphaelarenalesv@gmail.com>
2025-01-11 20:31:10 -0500

    Cuarto cambio correcto en EL PROYECTO FINAL

--- back/src/index.js
+++ back/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 require('dotenv').config();
 const estudiantesController = require('./controllers/estudiantesController');
+const monitoreoController = require('./controllers/monitoreoController');
+
 
 const app = express();
 app.use(express.json());
@@ -12,7 +14,9 @@ console.log(__dirname);
 console.log(require.resolve('./controllers/estudiantesController'));
 
 
+
 app.use('/api/estudiantes', estudiantesController);
+app.use('/api/monitoreo', monitoreoController);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
