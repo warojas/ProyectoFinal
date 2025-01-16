3f90ff8a4130b8cd7900d75a9cf9b10cfb98c2ae
warojas <warojas1@gmail.com>
2025-01-16 02:09:36 -0500

    feat: plantilla-1

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LitarAlumnoComponent } from './components/litar-alumno/litar-alumno.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { LitarAlumnoComponent } from './components/litar-alumno/litar-alumno.com
     LitarAlumnoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
